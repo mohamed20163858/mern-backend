d56e50d67540f39a8407949eae02c0c30ff46548
Mohamed Saleh <mohamed20163858@gmail.com>
2024-01-08 17:52:56 +0200

    Modify create method error message

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -24,6 +24,19 @@ const getWorkout = async (req, res) => {
 // post a single workout
 const createWorkout = async (req, res) => {
     const {title, reps, load} = req.body;
+    const emptyFields = [];
+    if(!title) {
+        emptyFields.push('title');
+    }
+    if(!reps) {
+        emptyFields.push('reps');
+    }
+    if(!load) {
+        emptyFields.push('load');
+    }
+    if( emptyFields.length > 0) {
+        return res.status(400).json({error: 'please fill in all fields', emptyFields})
+    }
      try{
         const workout = await Workout.create({title, reps, load});
         res.status(200).json(workout);
